3d6ac41ff60995838755bfcb209f89391e329f14
“rohyapawar2912” <rohitpawar8530@gmail.com>
2023-04-18 10:27:50 +0530

    add : day03 all files

---
+++ Day03/Line.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Line : IComparable<Line>
+    {
+
+        public double x1 { get; set; }
+        public double y1 { get; set; }
+        public double x2 { get; set; }
+        public double y2 { get; set; }
+
+        // Constructor
+        public Line(double x1, double y1, double x2, double y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        // Implement CompareTo method to compare lines based on endpoint coordinates
+        public int CompareTo(Line other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+
+            // Compare lines based on their endpoint coordinates
+            int compare = x1.CompareTo(other.x1);
+            if (compare != 0)
+            {
+                return compare;
+            }
+
+            compare = y1.CompareTo(other.y1);
+            if (compare != 0)
+            {
+                return compare;
+            }
+
+            compare = x2.CompareTo(other.x2);
+            if (compare != 0)
+            {
+                return compare;
+            }
+
+            return y2.CompareTo(other.y2);
+        }
+
+
+    }
+}
---
+++ Day03/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace practice
+{
+    internal class Program
+    {
+            static void Main(string[] args)
+            {
+                {
+                    // Create two lines
+                    Line line1 = new Line(0, 0, 1, 1);
+                    Line line2 = new Line(0, 1, 1, 0);
+
+                    // Compare lines using CompareTo method
+                    int compareResult = line1.CompareTo(line2);
+
+                    // Output the result
+                    if (compareResult < 0)
+                    {
+                        Console.WriteLine("Line 1 is less than Line 2");
+                    }
+                    else if (compareResult > 0)
+                    {
+                        Console.WriteLine("Line 1 is greater than Line 2");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Line 1 is equal to Line 2");
+                    }
+                }
+            }
+        
+    }
+}
